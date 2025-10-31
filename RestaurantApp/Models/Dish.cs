67d0bd5a6e8ff629abe3e4b00857ca1cb15a7893
Maciej Choroś <maciejchoros@elektronikonline.pl>
2025-10-31 16:29:22 +0100

    Utworzono model Dish

---
+++ RestaurantApp/Models/Dish.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace RestaurantApp.Models
+{
+    public class Dish
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nazwa dania jest wymagana")]
+        [StringLength(100)]
+        public required string Name { get; set; }
+
+        [Required(ErrorMessage = "Opis jest wymagany")]
+        [StringLength(500)]
+        public required string Description { get; set; }
+
+        [Required(ErrorMessage = "Cena jest wymagana")]
+        [Column(TypeName = "decimal(10,2)")]
+        [Range(0.01, 10000.00, ErrorMessage = "Cena musi być większa od 0")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Kategoria jest wymagana")]
+        public MealType Category { get; set; }
+
+        [StringLength(255)]
+        public string? ImagePath { get; set; } 
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+    public enum MealType
+    {
+        Śniadanie = 1,
+        Obiad = 2,
+        Kolacja = 3
+    }
+}
